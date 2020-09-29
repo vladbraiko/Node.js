6c30069db90670e8cb73eb5a21deb287046fc96f
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-09-29 22:50:39 +0300

    added mongoDB and set up a connection through mongoose

--- api/contacts/routes.js
+++ api/contacts/routes.js
@@ -1,50 +1,59 @@
 const { Router } = require("express");
-const Contacts = require("../../contacts");
+
+const {
+  getContactController,
+  createContactController,
+  updateContactController,
+  deleteContactController,
+} = require("./contacts.controller");
 
 const contactsRouter = Router();
 
-contactsRouter.get("/", async (req, res) => {
-  const contacts = await Contacts.listContacts();
-  res.status(200).json(contacts);
-});
+contactsRouter.get("/", getContactController);
 
-contactsRouter.get("/:contactId", async (req, res) => {
-  const contactId = Number(req.params.contactId);
-  const contact = await Contacts.getContactById(contactId);
-  if (!contact) res.status(404).json({ message: "Not found" });
-  res.status(200).json(contact);
-  return;
-});
+contactsRouter.post("/", createContactController);
 
-contactsRouter.post("/", async (req, res) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone)
-    return res.status(400).json({ message: "missing required name field" });
-  const newContact = await Contacts.addContact(name, email, phone);
-  res.status(201).json(newContact);
-});
+contactsRouter.patch("/", updateContactController);
 
-contactsRouter.delete("/:contactId", async (req, res) => {
-  const id = Number(req.params.contactId);
-  const contact = await Contacts.getContactById(id);
-  if (!contact) return res.status(404).json({ message: "Not found" });
+contactsRouter.delete("/:contactId", deleteContactController);
 
-  await Contacts.removeContact(id);
-  res.status(200).json({ message: "contact deleted" });
-  return;
-});
+// contactsRouter.get("/:contactId", async (req, res) => {
+//   const contactId = Number(req.params.contactId);
+//   const contact = await Contacts.getContactById(contactId);
+//   if (!contact) res.status(404).json({ message: "Not found" });
+//   res.status(200).json(contact);
+//   return;
+// });
 
-contactsRouter.patch("/:contactId", async (req, res) => {
-  const { body } = req;
-  if (!body) return res.status(400).json({ message: "missing field" });
+// contactsRouter.post("/", async (req, res) => {
+//   const { name, email, phone } = req.body;
+//   if (!name || !email || !phone)
+//     return res.status(400).json({ message: "missing required name field" });
+//   const newContact = await Contacts.addContact(name, email, phone);
+//   res.status(201).json(newContact);
+// });
 
-  const id = Number(req.params.contactId);
-  const contact = await Contacts.getContactById(id);
-  if (!contact) return res.status(404).json({ message: "Not found" });
+// contactsRouter.delete("/:contactId", async (req, res) => {
+//   const id = Number(req.params.contactId);
+//   const contact = await Contacts.getContactById(id);
+//   if (!contact) return res.status(404).json({ message: "Not found" });
 
-  const updatedContact = await Contacts.updateContact(id, body);
+//   await Contacts.removeContact(id);
+//   res.status(200).json({ message: "contact deleted" });
+//   return;
+// });
 
-  res.status(200).json(updatedContact);
-});
+// contactsRouter.patch("/:contactId", async (req, res) => {
+//   const { body } = req;
+//   if (!body) return res.status(400).json({ message: "missing field" });
+
+//   const id = Number(req.params.contactId);
+//   const contact = await Contacts.getContactById(id);
+//   if (!contact) return res.status(404).json({ message: "Not found" });
+
+//   const updatedContact = await Contacts.updateContact(id, body);
+
+//   res.status(200).json(updatedContact);
+// });
 
 module.exports = contactsRouter;
